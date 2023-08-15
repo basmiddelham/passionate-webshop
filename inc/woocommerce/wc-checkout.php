68f6cc7126be8361df4136ac1133b1e75697bac2
Bas Middelham <bas@middelham.nl>
2023-08-15 12:36:03 +0200

    Update

--- inc/woocommerce/wc-checkout.php
+++ inc/woocommerce/wc-checkout.php
@@ -10,44 +10,62 @@
 /**
  * Customize WooCommerce Checkout fields.
  */
-// function strt_checkout_fields( $fields ) {
-// 	// Add placeholders.
-// 	$fields['billing']['billing_email']['placeholder'] = 'E-mailadres';
-// 	$fields['billing']['billing_phone']['placeholder'] = 'Telefoon';
+function strt_checkout_fields( $fields ) {
+	// Add gender field to checkout page to personalize emails.
+	$billing_gender    = [
+		'billing_gender' => [
+			'type'        => 'select',
+			'label'       => 'Aanhef',
+			'required'    => true,
+			'class'       => array( 'select form-row-wide' ),
+			'clear'       => true,
+			'options'     => array(
+				'' => 'Aanhef',
+				'female' => 'Mevr.',
+				'male'   => 'Dhr.',
+			)
+		],
+	];
+	$fields['billing'] = array_merge( $billing_gender, $fields['billing'] );
 
-// 	// Change layout of phone and email fields.
-// 	$fields['billing']['billing_phone']['priority'] = 110;
-// 	$fields['billing']['billing_email']['priority'] = 100;
-// 	$fields['billing']['billing_phone']['class'][0] = 'form-row-last';
-// 	$fields['billing']['billing_email']['class'][0] = 'form-row-first';
+		// Add placeholders.
+	$fields['billing']['billing_email']['placeholder'] = 'E-mailadres*';
+	$fields['billing']['billing_phone']['placeholder'] = 'Telefoon (optioneel)';
 
-// 	// Make fields optional.
-// 	$fields['billing']['billing_phone']['required'] = false;
+	// Change layout of phone and email fields.
+	$fields['billing']['billing_phone']['priority'] = 110;
+	$fields['billing']['billing_email']['priority'] = 100;
+	$fields['billing']['billing_phone']['class'][0] = 'form-row-last';
+	$fields['billing']['billing_email']['class'][0] = 'form-row-first';
 
-// 	return $fields;
-// }
-// add_filter( 'woocommerce_checkout_fields' , 'strt_checkout_fields' );
+	// Make fields optional.
+	$fields['billing']['billing_phone']['required'] = false;
+
+	return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'strt_checkout_fields' );
 
 /**
  * Customize WooCommerce Address Checkout fields.
  */
-// function strt_default_address_fields( $fields ) {
-// 	// Remove fields.
-// 	unset( $fields['address_2'] );
+function strt_default_address_fields( $fields ) {
+	// Remove fields.
+	unset( $fields['address_2'] );
 
-// 	// Placeholders.
-// 	$fields['postcode']['placeholder']   = 'Postcode';
-// 	$fields['city']['placeholder']       = 'Plaats';
-// 	$fields['first_name']['placeholder'] = 'Voornaam';
-// 	$fields['last_name']['placeholder']  = 'Achternaam';
-// 	$fields['company']['placeholder']    = 'Bedrijf (optioneel)';
+	// Placeholders.
+	$fields['first_name']['placeholder'] = 'Voornaam*';
+	$fields['last_name']['placeholder']  = 'Achternaam*';
+	$fields['address_1']['placeholder']  = 'Straat en huisnummer*';
+	$fields['postcode']['placeholder']   = 'Postcode*';
+	$fields['city']['placeholder']       = 'Plaats*';
+	$fields['company']['placeholder']    = 'Bedrijf (optioneel)';
 
-// 	// Change postcode and city layout.
-// 	if ( is_checkout() ) {
-// 		$fields['postcode']['class'][0] = 'form-row-first';
-// 		$fields['city']['class'][0]     = 'form-row-last';
-// 	}
+	// Change postcode and city layout.
+	if ( is_checkout() ) {
+		$fields['postcode']['class'][0] = 'form-row-first';
+		$fields['city']['class'][0]     = 'form-row-last';
+	}
 
-// 	return $fields;
-// }
-// add_filter('woocommerce_default_address_fields', 'strt_default_address_fields', 20);
+	return $fields;
+}
+add_filter( 'woocommerce_default_address_fields', 'strt_default_address_fields', 20 );
