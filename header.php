145f452a9f7fdf9a96d05acf8faf731563f5082e
Bas Middelham <bas@middelham.nl>
2024-06-21 12:12:48 +0200

    Add Meta pixel

--- header.php
+++ header.php
@@ -26,6 +26,24 @@
 	<meta name="msapplication-TileColor" content="#dce9d7">
 	<meta name="msapplication-config" content="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/favicon/browserconfig.xml">
 	<meta name="theme-color" content="#dce9d7">
+	<meta name="facebook-domain-verification" content="q7ol1996tcziwtq6t2o5xc7g8bare1" />
+	<!-- Meta Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '2587413514771259');
+	fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=2587413514771259&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Meta Pixel Code -->
 </head>
 
 <body <?php body_class(); ?>>
